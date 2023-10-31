6d4188d55b794d3da13d02ed70ed05ee9e967108
Tulika Chaudharie <tulikac@microsoft.com>
2023-10-31 21:29:13 +0530

    changing the constructor again

--- src/main/java/com/example/grpc/GreetingServiceImpl.java
+++ src/main/java/com/example/grpc/GreetingServiceImpl.java
@@ -66,14 +66,18 @@ public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImpl
 	
 
     // START - OpenAI Chat //
-    OpenAIClient client = new OpenAIClient(
-        new java.net.URI("https://ignite-demo-apim.azure-api.net/deployments/deploy1/chat/completions?api-version=INSERT_API_VERSION_HERE"),
-        new AzureKeyCredential("e448e88135da4b30ab51baf117cae890"));
 
-		//OpenAIClient client = new OpenAIClientBuilder()
-		//	.credential(new AzureKeyCredential("dc7b76ac36874ea0aef8674840e31eab"))
-		//	.endpoint("https://ignite-demo-openai.openai.azure.com/")
-		//	.buildClient();
+
+    //OpenAIClient client = new OpenAIClient(
+      //  new java.net.URI("https://ignite-demo-apim.azure-api.net/deployments/deploy1/chat/completions?api-version=INSERT_API_VERSION_HERE"),
+        //new AzureKeyCredential("e448e88135da4b30ab51baf117cae890"));
+
+        
+
+		OpenAIClient client = new OpenAIClientBuilder()
+			.credential(new AzureKeyCredential("e448e88135da4b30ab51baf117cae890"))
+			.endpoint("https://ignite-demo-apim.azure-api.net/deployments/deploy1/chat/completions?api-version=2023-07-01-preview")
+			.buildClient();
 
     // qData: data set from queue storage account
     String systemContext = qData;
